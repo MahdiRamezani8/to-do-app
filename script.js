7234a474da17bfc21dc98f0fd0758b0ec2ba8220
Mahdi ramezani <ramezani86mraz@gmail.com>
2023-03-15 11:24:12 +0330

    i added my codes

---
+++ script.js
@@ -0,0 +1,60 @@
+var $ = document;
+let addBtn = $.getElementById('add')
+let toDoes = $.getElementById('to-does')
+let text = $.getElementById('text')
+let clear = $.getElementById('empty')
+let colors = $.querySelectorAll('.color')
+let color = $.getElementsByClassName('color')
+let list = $.getElementsByClassName('to-do')
+var sum = ""
+
+// گرفتن رنگی که برای اینپوت انتخاب کردیم 
+colors.forEach((item , index) => {
+    item.addEventListener("click" , () => {
+        sum = color[index].style.backgroundColor;
+        text.style.backgroundColor = sum
+    })
+})
+
+// clear input
+clear.addEventListener('click', () => {
+    text.value = ""
+    text.style.backgroundColor = "white"
+    sum = "#aaa"
+})
+
+function addLi() {      
+    if (text.value == "") {
+        return false;
+    }
+    let liElem = $.createElement('li');
+    liElem.className = "to-do"
+    liElem.innerHTML = text.value
+    liElem.style.backgroundColor = sum
+
+    let iconElem = $.createElement('i');
+    iconElem.className = "fa fa-minus-square ielem"
+
+
+    text.value = ""
+    sum = "#aaa"
+    text.style.backgroundColor = "#fff"
+
+    liElem.append(iconElem) 
+    toDoes.append(liElem)
+
+    let removeIcon = Array.from($.querySelectorAll('.ielem'))
+    removeIcon.forEach(element => {
+        element.addEventListener('click', () => {
+          element.parentElement.remove()
+        })
+    });
+}
+
+$.body.addEventListener('keypress', () => {
+    if (event.keyCode == 13) {
+        addLi()
+    }
+})
+
+addBtn.addEventListener('click', addLi)
